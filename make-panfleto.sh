490cac4b03045bbe4b4d4610e8cea120b4e1310c
Clara de Andrade <clara.andrade@protonmail.com>
2026-03-07 09:50:53 -0300

    Pass PDF filename as argument instead

--- make-panfleto.sh
+++ make-panfleto.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/bash
 
-set -x
+# argument is doc
+DOC=$1
 
 # requires latexmk and pdfjam
 command -v latexmk pdfjam >/dev/null 2>&1 || exit 1
 
-# make rosario.pdf
-latexmk -f -pdf './rosario.tex'
+# make base document
+latexmk -f -pdf "${DOC}.tex"
 
-# make rosario-panfleto.pdf
-pdfjam -o './rosario-panfleto.pdf'  --booklet true --landscape --a4paper -- './rosario.pdf'
+# make booklet
+pdfjam -o "${DOC}-panfleto.pdf"  --booklet true --landscape --a4paper -- "${DOC}.pdf"
 
 # cleanup files
 latexmk -C
+
+# symlink booklet
+ln -sf "${DOC}-panfleto.pdf" "${DOC}.pdf"
