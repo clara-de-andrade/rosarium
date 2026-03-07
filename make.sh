a41f3536375ff0fbb3197d48874dd371dc377080
Clara de Andrade <clara.andrade@protonmail.com>
2026-03-07 11:27:49 -0300

    Fixed typos, changed make script

--- make.sh
+++ make.sh
@@ -8,12 +8,7 @@ command -v latexmk pdfjam >/dev/null 2>&1 || exit 1
 
 # make base document
 latexmk -f -pdf "${DOC}.tex"
+latexmk -c
 
 # make booklet
-pdfjam -o "${DOC}-panfleto.pdf"  --booklet true --landscape --a4paper -- "${DOC}.pdf"
-
-# cleanup files
-latexmk -C
-
-# symlink booklet
-ln -sf "${DOC}-panfleto.pdf" "${DOC}.pdf"
+pdfjam -o "${DOC}.pdf"  --booklet true --landscape --a4paper -- "${DOC}.pdf"
